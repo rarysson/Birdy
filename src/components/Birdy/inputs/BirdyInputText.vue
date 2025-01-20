89a46c2e237ba01dd7d1f23810e6951fd206b297
Rarysson Guilherme <r_guilherme12@hotmail.com>
2025-01-20 20:19:58 -0300

    feat: add text input component

---
+++ src/components/Birdy/inputs/BirdyInputText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id?: string
+}>()
+
+const model = defineModel()
+</script>
+
+<template>
+  <input
+    class="border border-zinc-500 rounded-lg h-10 px-4 py-2"
+    type="text"
+    :id="props.id"
+    v-model="model"
+  />
+</template>
