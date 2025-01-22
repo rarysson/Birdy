d32c9acd732b83caf6f920faeea6eaadb7627cf4
Rarysson Guilherme <r_guilherme12@hotmail.com>
2025-01-22 16:42:04 -0300

    feat: add latest searches

--- src/components/Birdy/BirdyHeader.vue
+++ src/components/Birdy/BirdyHeader.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import ProjectSearch from '../ProjectSearch.vue'
-import { useSearches } from '@/stores/searches'
+import { useSearches, type Search } from '@/stores/searches'
 import BirdyLogo from './BirdyLogo.vue'
+import BirdyButton from './BirdyButton.vue'
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
 
 const route = useRoute()
 const searches = useSearches()
+
+const latestSearchesHTMLRef = ref(null)
+const latestSearchesVisible = ref(false)
+
+function handleSelectSearch(search: Search) {
+  searches.removeSearch(search.id)
+  searches.searchTerm = search.term
+}
+
+onClickOutside(latestSearchesHTMLRef, () => (latestSearchesVisible.value = false))
 </script>
 
 <template>
@@ -20,7 +33,15 @@ const searches = useSearches()
     <ProjectSearch v-if="route.name !== 'Project'" />
     <div v-else><!-- empty space --></div>
   </header>
-  <header v-else class="drop-shadow-md bg-white flex text-center justify-between items-center p-8">
+  <header
+    v-else
+    class="relative drop-shadow-md bg-white flex text-center justify-between items-center p-8 z-10"
+    :class="{
+      'rounded-t-2xl border-2 border-b-0 border-birdy-300':
+        latestSearchesVisible && searches.latestSearches.length,
+    }"
+    ref="latestSearchesHTMLRef"
+  >
     <form class="flex items-center gap-6 w-full" @submit.prevent="">
       <label for="search">
         <img
@@ -31,8 +52,37 @@ const searches = useSearches()
           height="20"
         />
       </label>
-      <input class="w-full" id="search" type="text" v-model="searches.searchTerm" />
+      <input
+        class="w-full"
+        id="search"
+        type="text"
+        v-model="searches.searchTerm"
+        @click.stop="latestSearchesVisible = !latestSearchesVisible"
+      />
     </form>
+
+    <ul
+      v-show="latestSearchesVisible && searches.latestSearches.length"
+      class="absolute -left-[2px] top-full h-fit bg-white w-[calc(100%_+_4px)] rounded-b-2xl border-2 border-t-0 border-birdy-300"
+    >
+      <li
+        v-for="search in searches.latestSearches"
+        :key="search.id"
+        class="flex border-t border-t-[#f4f2ff] px-6 py-4"
+      >
+        <button class="w-full flex gap-4 items-center" @click="handleSelectSearch(search)">
+          <img src="@/assets/icons/last-search.svg" alt="icon" width="16" height="16" />
+          {{ search.term }}
+        </button>
+
+        <BirdyButton
+          icon="remove"
+          type="transparent"
+          size="xs"
+          @click="searches.removeSearch(search.id)"
+        />
+      </li>
+    </ul>
   </header>
 </template>
 
--- src/stores/searches.ts
+++ src/stores/searches.ts
@@ -1,27 +1,48 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { ref, watch } from 'vue'
 import { db, type Project } from '@/db'
 import { refDebounced } from '@vueuse/core'
+
+export type Search = {
+  id: number
+  term: string
+}
 
 const _projects = ref<Project[]>([])
 
 export const useSearches = defineStore('searches', () => {
   const searchTerm = ref('')
-  const _term = refDebounced(searchTerm, 250)
+  const latestSearches = ref<Search[]>([])
+  const projects = ref<Project[]>([])
 
-  const projects = computed(() => {
-    if (_term.value.length >= 3) {
-      return _projects.value.filter((p) => p.name.includes(_term.value))
-    } else {
-      return []
-    }
-  })
+  const _term = refDebounced(searchTerm, 250)
 
   async function fillProjects() {
     _projects.value = await db.projects.toArray()
   }
 
-  return { searchTerm, projects, fillProjects }
+  function removeSearch(id: number) {
+    const index = latestSearches.value.findIndex((s) => s.id === id)
+
+    if (index !== -1) {
+      latestSearches.value.splice(index, 1)
+    }
+  }
+
+  watch(_term, (term) => {
+    if (term.length >= 3) {
+      latestSearches.value = [
+        { id: Date.now(), term: _term.value },
+        ...latestSearches.value.slice(0, 4),
+      ]
+
+      projects.value = _projects.value.filter((p) => p.name.includes(term))
+    } else {
+      projects.value = []
+    }
+  })
+
+  return { searchTerm, projects, latestSearches, fillProjects, removeSearch }
 })
 
 if (import.meta.hot) {
